9c79dd2b4925925bade30ee12c248ef2463777ec
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-08-06 14:00:55 -0500

    created tables

--- GNS.sql
+++ GNS.sql
@@ -1,1 +1,35 @@
 -- comment queries
+
+CREATE TABLE GNS(
+    GCS TEXT NOT NULL,
+    combinedPT TEXT NOT NULL,
+    combinedPC TEXT NOT NULL,
+    PT TEXT NOT NULL,
+    PC TEXT NOT NULL
+    PRIMARY KEY (PT)
+    )
+
+CREATE TABLE arev (
+    DPN VARCHAR(5) NOT NULL,
+    Descrip TEXT NOT NULL,
+    PT TEXT NOT NULL,
+    PC TEXT NOT NULL,
+    Phase TEXT NOT NULL
+    UNIQUE (DPN)
+    FOREIGN KEY (PT) REFERENCES GCS (PT)
+)
+
+CREATE TABLE xrev (
+    DPN VARCHAR(5) NOT NULL,
+    Descrip TEXT NOT NULL,
+    PT TEXT NOT NULL,
+    PC TEXT NOT NULL,
+    Phase TEXT NOT NULL
+    UNIQUE (DPN)
+    FOREIGN KEY (PT) REFERENCES GCS (PT)
+)
+
+SELECT * FROM arev
+
+-- Data cleanse
+-- remove all non xirev from life cycle phase; keep 'X Revision'
--- queries.sql
+++ queries.sql
@@ -1,1 +1,35 @@
--- comment
+CREATE TABLE GNS(
+    GCS TEXT NOT NULL,
+    combinedPT TEXT NOT NULL,
+    combinedPC TEXT NOT NULL,
+    PT TEXT NOT NULL,
+    PC TEXT NOT NULL,
+    PRIMARY KEY (PT)
+    );
+
+CREATE TABLE arev (
+    DPN VARCHAR(5) NOT NULL,
+    Descrip TEXT NOT NULL,
+    PT TEXT NOT NULL,
+    PC TEXT NOT NULL,
+    Phase TEXT NOT NULL,
+    UNIQUE (DPN),
+    FOREIGN KEY (PT) REFERENCES GNS (PT)
+);
+
+CREATE TABLE xrev (
+    DPN VARCHAR(5) NOT NULL,
+    Descrip TEXT NOT NULL,
+    PT TEXT NOT NULL,
+    PC TEXT NOT NULL,
+    Phase TEXT NOT NULL,
+    UNIQUE (DPN),
+    FOREIGN KEY (PT) REFERENCES GNS (PT)
+);
+
+SELECT * FROM GNS;
+
+-- Data cleanse
+-- remove all non x-rev from life cycle phase; keep 'X Revision'
+
+
