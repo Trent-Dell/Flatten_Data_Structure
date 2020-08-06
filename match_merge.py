4a24463562c58617a871aff5ce8decd4a434fbc9
Trent-Dell <69316342+Trent-Dell@users.noreply.github.com>
2020-08-06 13:10:50 -0500

    python

---
+++ match_merge.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+
+file_path = 'C:/Users/trent_brunson/Documents/GitHub/Flatten_Data_Structure/'
+
+arev = 'PARTS_arev.csv'
+
+xrev = 'PARTS_xrev.csv'
+
+# load GNS table, to arev or xrev reports
+    # ask for file location
+
+# match arev and xrev to GNS reference table
+
+# export new arev and xrev reports
