9279084e88b1a01c91b69787b08cdcd1ef583fa8
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-08-07 10:39:16 -0500

    attributes appended to all valid assets

--- match_merge.py
+++ match_merge.py
@@ -1,3 +1,4 @@
+#%%
 import os
 import pandas as pd
 
@@ -7,6 +8,25 @@ arev = 'PARTS_arev.csv'
 
 xrev = 'PARTS_xrev.csv'
 
+#%%
+# clean files for arev/xrev; export to cleaned file
+    # load into PD 
+    # verify Arev/Xrev status
+        # drop statuses that don't match
+    # drop dupes for DPN
+    # find nulls in PT
+        # drop if any
+    # find nulls in description, fill with 'NAN'
+    # with {file} open (w)
+        # list comprehension: for DPN in DPN
+            # if arev/xrev[PT] = gns[PT]
+            #   ...append new attributes
+            # else 
+                # invalid.append()
+
+
+
+#%%
 # load GNS table, to arev or xrev reports
     # ask for file location
 
--- queries.sql
+++ queries.sql
@@ -15,8 +15,8 @@ CREATE TABLE arev (
     PC TEXT NOT NULL,
     PT TEXT NOT NULL,
     Phase TEXT NOT NULL,
-    UNIQUE (DPN),
-    FOREIGN KEY (PT) REFERENCES GNS (PT)
+    UNIQUE (DPN)
+    -- FOREIGN KEY (PT) REFERENCES GNS (PT)
 );
 
 CREATE TABLE xrev (
@@ -48,20 +48,16 @@ SELECT DPN,length(DPN) as "valid DPN" from xrev
 DELETE FROM XREV WHERE length(DPN) > 5;
 -- deleted 30610; 182747 remaining
 
-
 SELECT COUNT (PT) from XREV where (PT) is NULL;
 
+SELECT * INTO Xclean FROM XREV
+	WHERE (PHASE = 'X Revision') AND (length(DPN) < 6);
 
-
-SELECT PHASE 
-INTO Xclean
-FROM XREV
-WHERE (PHASE = 'X Revision')
-AND (DPN)
+SELECT * FROM Xclean;
 
 drop table gns cascade;
 
-SELECT * FROM XREV LIMIT 100;
+SELECT * FROM aREV LIMIT 100;
 
 SELECT COUNT(PT) FROM GNS;
 
@@ -69,3 +65,50 @@ SELECT COUNT(PT) FROM xrev where "PT" is Null;
 SELECT COUNT(dpn) FROM xrev;
 
 SELECT COUNT(DPN) FROM arev;
+
+-- clean arev, export as aclean
+SELECT COUNT (DPN) FROM AREV;
+	-- returned 1457511
+SELECT COUNT (PT) from aREV where (PT) is NULL; -- 0 nulls for PT
+SELECT COUNT (PHASE) FROM AREV WHERE "phase" = "A Revison";
+	-- returned 1457511; the total count
+SELECT DPN,length(DPN) as "valid DPN" from arev
+	where length(DPN)<6;
+-- 1393712 returned
+SELECT * from "valid DPN";
+
+DELETE FROM AREV WHERE Phase <> 'A Revision';
+	-- 0 deleted, ALL were Arev
+DELETE FROM AREV WHERE length(DPN) > 5;
+-- deleted 63799; 1393712 remaining
+SELECT * FROM AREV;
+
+CREATE TABLE arevCleaned (
+    -- DPN VARCHAR(5) NOT NULL, input file includes non DPN & non parts
+    DPN TEXT NOT NULL,
+    Descrip TEXT NOT NULL,
+    PC TEXT NOT NULL,
+    PT TEXT NOT NULL,
+    Phase TEXT NOT NULL,
+    UNIQUE (DPN),
+    FOREIGN KEY (PT) REFERENCES GNS (PT)
+);
+
+SELECT * INTO aclean FROM aREV
+	WHERE (PHASE = 'A Revision') AND (length(DPN) < 6);
+select * from aclean;
+-- join arev/xrev to GNS
+	-- append attributes to arev/xrev
+SELECT xr.dpn, xr.pc, xr.pt, gcs, combinedpt, combinedpc
+INTO Xrev_newAttr FROM gns
+LEFT JOIN xclean as xr ON (gns.pt = xr.pt);
+
+SELECT * FROM Xrev_newAttr;
+	-- 181127
+
+SELECT ar.dpn, ar.pc, ar.pt, gcs, combinedpt, combinedpc
+INTO Arev_newAttr FROM gns
+LEFT JOIN aclean as ar ON (gns.pt = ar.pt);
+
+SELECT * FROM Arev_newAttr;
+	-- 1390244
